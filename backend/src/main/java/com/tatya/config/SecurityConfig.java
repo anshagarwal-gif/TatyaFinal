746506b7fcab24c4f7183ea248ce678461f19ded
root <root@srv1295909.hstgr.cloud>
2026-01-26 20:59:40 +0000

    Chnages in application.properties

--- backend/src/main/java/com/tatya/config/SecurityConfig.java
+++ backend/src/main/java/com/tatya/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -29,6 +31,13 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
